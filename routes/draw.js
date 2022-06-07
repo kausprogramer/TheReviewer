43f3e53ca16c6920b2fc6f102af02f193ca00501
kausprogramer <kos2bh@gmail.com>
2022-06-07 13:28:45 +0530

    commit

--- routes/draw.js
+++ routes/draw.js
@@ -6,7 +6,7 @@ import {createAppContainer} from 'react-navigation';
 import AboutStack from './aboutstack';
 import homeStack from './homeStack';
 
-const RootDrawerNavigator = createDrawerContainer({
+const RootDrawerNavigator = createDrawerNavigator({
 Home:{
     screen:homeStack,
 },
