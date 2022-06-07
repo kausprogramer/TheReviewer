8b711b0e080986d9a26ecff6bf9aef1a9d2ba67d
kausprogramer <kos2bh@gmail.com>
2022-06-07 21:57:14 +0530

    commit

--- shared/header.js
+++ shared/header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {StyleSheet,Text,View}  from 'react-native';
 import {MaterialIcons} from '@expo/vector-icons';
+import { NavigationContext } from "react-navigation";
 
 export default function Header() {
+    const openMenu=() => {
+        navigation.openDrawer()
+    }
     return(
         <View styles={Styles.header}>
             <View>
-                <MaterialIcons name ='menu' size={28}
+                <MaterialIcons name ='menu' size={28} onPress={openMenu} style={Styles.icon}/>
                 <Text style={Styles.headerText}>The Reviewer</Text>
             </View>
         </View>
